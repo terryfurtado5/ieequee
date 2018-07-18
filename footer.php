dd9b045bc947ceae85b49a43f5ff7303bea7d654
terryfurtado5 <41382189+terryfurtado5@users.noreply.github.com>
2018-07-18 16:15:17 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,52 @@
+<div class="main-footer">
+				
+					<div class="container">
+					
+						<div class="row">
+						
+							<div class="col-sm-12 col-md-9">
+							
+                        <h2>Reach millions of People</h2>
+                        <p>Add your Business infront of millions and earn 3x profits from our Pinpoint</p>
+                    </div>
+                </div>
+            </div>
+                    <div class="featured-btn-wrap">
+                        <a href="./employer/index.php" class="btn btn-info"><span class="ti-plus"></span> + ADD PINPOINT</a>
+                    </div>
+						
+					</div>
+				
+				<div class="bottom-footer">
+				
+					<div class="container">
+					
+						<div class="row">
+						
+							<div class="col-sm-4 col-md-4">
+					
+								<p class="copy-right">&#169; Copyright <?php echo date('Y'); ?> coded</p>
+								
+							</div>
+							
+							<div class="col-sm-4 col-md-4">
+							
+								<ul class="bottom-footer-menu">
+									<li><a >Developed by IQ</a></li>
+								</ul>
+							
+							</div>
+							
+							<div class="col-sm-4 col-md-4">
+								<ul class="bottom-footer-menu for-social">
+									<li><a href="<?php echo "$tw"; ?>"><i class="ri ri-twitter" data-toggle="tooltip" data-placement="top" title="twitter"></i></a></li>
+									<li><a href="<?php echo "$fb"; ?>"><i class="ri ri-facebook" data-toggle="tooltip" data-placement="top" title="facebook"></i></a></li>
+									<li><a href="<?php echo "$ig"; ?>"><i class="ri ri-instagram" data-toggle="tooltip" data-placement="top" title="instagram"></i></a></li>
+								</ul>
+							</div>
+						
+						</div>
+
+					</div>
+					
+				</div>
---
+++ header.php
@@ -0,0 +1,107 @@
+<body class="home">
+
+
+	<div id="introLoader" class="introLoading"></div>
+
+	<div class="container-wrapper">
+
+		<header id="header"> 
+
+			<nav class="navbar navbar-default navbar-fixed-top navbar-sticky-function">
+
+				<div class="container">
+					
+					<div class="logo-wrapper">
+						<div class="logo">
+							<a href="./"><img src="image/LOGO.png" alt="Logo"/></a>
+						</div>
+					</div>
+					
+					<div id="navbar" class="navbar-nav-wrapper navbar-arrow">
+					
+						<ul class="nav navbar-nav" id="responsive-menu">
+						
+							<li>
+							
+								<a href="home.php">Home</a>
+								
+							</li>
+							
+							<li>
+								<a href="adverts.php">Pinpoint Stories </a>
+
+							</li>
+							<li>
+								<a href="hotspots.php">Tickets</a>
+							</li>
+							
+							
+							<li>
+								<a href="blog.php">Blog</a>
+							</li>
+							
+							<li>
+								<div class="featured-btn-wrap">
+                        <a href="./employer/index.php" class="btn btn-info"><span class="ti-plus"></span> + ADD PINPOINT</a>
+                    </div>
+							</li>
+
+						</ul>
+				
+					</div>
+
+					<div class="nav-mini-wrapper">
+						<ul class="nav-mini sign-in">
+						<?php
+						if ($user_online == true) {
+						print '
+						    <li><a href="logout.php">logout</a></li>
+							<li><a href="'.$myrole.'">Profile</a></li>';
+						}else{
+						print '
+							<li><a href="login.php">login</a></li>
+							<li><a data-toggle="modal" href="#registerModal">register</a></li>';						
+						}
+						
+						?>
+
+						</ul>
+					</div>
+				
+				</div>
+				
+				<div id="slicknav-mobile"></div>
+				
+			</nav>
+
+			
+			<div id="registerModal" class="modal fade login-box-wrapper" tabindex="-1" style="display: none;" data-backdrop="static" data-keyboard="false" data-replace="true">
+			
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title text-center">Create your account for free</h4>
+				</div>
+				
+				<div class="modal-body">
+				
+					<div class="row gap-20">
+					
+						<div class="col-sm-6 col-md-6">
+							<a href="register.php?p=Employer" class="btn btn-facebook btn-block mb-5-xs">Register as Vendor</a>
+						</div>
+						<div class="col-sm-6 col-md-6">
+							<a href="register.php?p=Employee" class="btn btn-facebook btn-block mb-5-xs">Register as Customer</a>
+						</div>
+
+					</div>
+				
+				</div>
+				
+				<div class="modal-footer text-center">
+					<button type="button" data-dismiss="modal" class="btn btn-primary btn-inverse">Close</button>
+				</div>
+				
+			</div>
+
+			
+		</header>
